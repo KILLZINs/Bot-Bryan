18ec4e297d2335ba81dd6be0ec0fe037abe2a08c
KILLZINs <103608072+KILLZINs@users.noreply.github.com>
2026-08-20 14:39:03 -0300

    Update calliaVoice.ts

--- src/voice/calliaVoice.ts
+++ src/voice/calliaVoice.ts
@@ -7,7 +7,7 @@ import {
   createAudioPlayer,
   createAudioResource,
   joinVoiceChannel,
-} from '@discordjs/voice';
+} from 'discord-voip'; // 💡 O SEGREDO ESTÁ AQUI: Trocamos o pacote!
 import { GuildMember, VoiceBasedChannel } from 'discord.js';
 import prism from 'prism-media';
 import { Readable } from 'node:stream';
